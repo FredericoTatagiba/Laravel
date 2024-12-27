ee634e8fa10798d86d2bd769455fc722e50e7a2b
Frederico Ferreira Tatagiba <fredericotatagiba@gmail.com>
2024-12-27 13:53:05 -0300

    Adicionado Breeze ao projeto para futuras implementações neste projeto.

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\DiscountController;
+use App\Http\Controllers\Api\ClientController;
+use App\Http\Controllers\Api\AdminController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\DiscountController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
