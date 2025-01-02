71b8295f1e3d4634f0c214a044504325bd7ac634
Frederico Ferreira Tatagiba <fredericotatagiba@gmail.com>
2025-01-02 10:19:15 -0300

    Adicionado Breeze ao projeto para futuras implementações neste projeto.

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\OrderFormRequest;
 use App\Http\Requests\OrderUpdateRequest;
 use App\Models\Order;
